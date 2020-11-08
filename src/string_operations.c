239dfec07719b1efa8b6dacb30cfbf20a9a0d6c3
Aakaaarsh <aakarsh.1421001.kiet.edu>
2020-11-08 16:09:24 +0530

    Trying for merging between branches

--- src/string_operations.c
+++ src/string_operations.c
@@ -1,5 +1,18 @@
 #include<string.h>
 #include<stdio.h>
+//aaakarshhh Jaaaainnnn
+char *my_strcat(char *p1,char *p2)
+{
+	while(*p1!=0)
+		p1++;
+	while(*p2!=0)
+	{
+		*p1=*p2;
+		p1++;
+		p2++;
+	}
+	 return *p1;
+}
 
 char *my_strcpy(char *t,char *s)
 {
@@ -25,6 +38,7 @@ int main()
 	{
 		printf("String length of %s =%d \n",s[i],my_strlen(s[i]));
 		printf("%s\n",my_strcpy(p1,"Hello, World"));
+		printf("$s\n",my_strcat("Hello","World"));
 		return 0;
 }
 
