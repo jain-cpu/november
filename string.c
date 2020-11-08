57c9d7caaf3b86aac8fb57f0cac830957139e203
Aakaaarsh <aakarsh.1421001.kiet.edu>
2020-11-08 14:37:06 +0530

    Changed return type from int to size_t by jerry

---
+++ string.c
@@ -0,0 +1,19 @@
+#include<string.h>
+#include<stdio.h>
+size_t my_strlen(char *s)
+{
+	char *p=s;
+	while(*p)
+		++p;
+	return(p-s);
+}
+
+int main()
+{
+	int i;
+	char *s[]={"Git tutorials","tutorials point"};
+	for(i=0;i<2;i++)
+		printf("String length of %s =%d \n",s[i],my_strlen(s[i]));
+	return 0;
+}
+
